3d1509cb09183ef2f8a2e5498c9575ef0930fa4b
Joinn99 <joinnnor@outlook.com>
2019-05-03 08:19:46 +0800

    Makefile

---
+++ build/CMakeFiles/KMeansSimmulator.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joinn/KMeans/build/KMeansSimmulator_autogen/mocs_compilation.cpp" "/home/joinn/KMeans/build/CMakeFiles/KMeansSimmulator.dir/KMeansSimmulator_autogen/mocs_compilation.cpp.o"
+  "/home/joinn/KMeans/build/qrc_kirinresource.cpp" "/home/joinn/KMeans/build/CMakeFiles/KMeansSimmulator.dir/qrc_kirinresource.cpp.o"
+  "/home/joinn/KMeans/src/kmeansdisplay.cpp" "/home/joinn/KMeans/build/CMakeFiles/KMeansSimmulator.dir/src/kmeansdisplay.cpp.o"
+  "/home/joinn/KMeans/src/kmeansobject.cpp" "/home/joinn/KMeans/build/CMakeFiles/KMeansSimmulator.dir/src/kmeansobject.cpp.o"
+  "/home/joinn/KMeans/src/main.cpp" "/home/joinn/KMeans/build/CMakeFiles/KMeansSimmulator.dir/src/main.cpp.o"
+  "/home/joinn/KMeans/src/mainwindow.cpp" "/home/joinn/KMeans/build/CMakeFiles/KMeansSimmulator.dir/src/mainwindow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "KMeansSimmulator_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/KMeansSimmulator.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "KMeansSimmulator_autogen"
+  "CMakeFiles/KMeansSimmulator_autogen.dir/AutogenOldSettings.cmake"
+  "ui_mainwindow.h"
+  "CMakeFiles/KMeansSimmulator.dir/src/kmeansdisplay.cpp.o"
+  "CMakeFiles/KMeansSimmulator.dir/src/kmeansobject.cpp.o"
+  "CMakeFiles/KMeansSimmulator.dir/src/main.cpp.o"
+  "CMakeFiles/KMeansSimmulator.dir/src/mainwindow.cpp.o"
+  "CMakeFiles/KMeansSimmulator.dir/qrc_kirinresource.cpp.o"
+  "CMakeFiles/KMeansSimmulator.dir/KMeansSimmulator_autogen/mocs_compilation.cpp.o"
+  "../bin/KMeansSimmulator.pdb"
+  "../bin/KMeansSimmulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KMeansSimmulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
